8988ae2fa5c195a1c783c4e2852ca04df4740a67
wells5609 <wellspeterson@live.com>
2014-02-23 20:33:41 -0500

    Create Provider.php

---
+++ src/Phpf/Util/Service/Provider.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @package Phpf.Util
+ * @subpackage Service.Provider
+ */
+
+namespace Phpf\Util\Service;
+
+class Provider implements \Php\Service\Provider {
+  
+	protected $provided = false;
+	
+	public function isProvided(){
+		return $this->provided;
+	}
+	
+	public function provide(){
+		
+		require dirname(__DIR__) . '/functions.php';
+		
+		$this->provided = true;
+	}
+	
+}
